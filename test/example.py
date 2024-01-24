88d1877bec0f927b7b6e7396410d5e7caf5f7073
George Korepanov <gkorepanov.gk@gmail.com>
2024-01-24 19:24:35 +0300

    Fix to strict segments skipping

--- test/example.py
+++ test/example.py
@@ -7,12 +7,16 @@ async def main():
     language, gen = await atranscribe_streaming_simple("test_audio.mp3", force_punctuation=True)
 
     print(language.name)
+    result = ""
 
     async for segment in gen:
         print(segment.text, end="", flush=True)
+        result += segment.text
+
+    print("\nResult:", result)
 
 
 if __name__ == "__main__":
     logging.getLogger("openai").setLevel(logging.WARNING)
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     asyncio.run(main())
